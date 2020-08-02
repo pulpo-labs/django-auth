0ebd8a24cba8d58382bc807b55978e2499023401
Ricardo Santos <ricardo.santos.diaz@gmail.com>
2020-08-02 23:00:48 +0200

    change to headers

--- buho/security/auth.py
+++ buho/security/auth.py
@@ -11,11 +11,11 @@ logger = logging.getLogger(__name__)
 class FederationAuthentication(BaseAuthentication):
     def authenticate(self, request):
         user = {}
-        user["id"] = request.META['X-User-Id']
-        user["first_name"] = request.META['X-User-FirstName']
-        user["last_name"] = request.META.get['X-User-LastName']
-        user["phone"] = request.META.get['X-User-Phone']
-        user["email"] = request.META.get['X-User-Email']
+        user["id"] = request.META['HTTP_X_USER_ID']
+        user["first_name"] = request.META['HTTP_X_USER_FIRSTNAME']
+        user["last_name"] = request.META.get['HTTP_X_USER_LASTNAME']
+        user["phone"] = request.META.get['HTTP_X_USER_PHONE']
+        user["email"] = request.META.get['HTTP_X_USER_EMAIL']
 
         user, __ = get_user_model().objects.get_or_create(
             username=user["email"],
