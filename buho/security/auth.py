e33664fcdbba4c63ed5bb84298051a4a9f02d65c
Ricardo Santos <ricardo.santos.diaz@gmail.com>
2021-03-15 21:39:08 +0100

    feat(project): add group handling to authenticate
    BREAKING CHANGE:

--- buho/security/auth.py
+++ buho/security/auth.py
@@ -2,7 +2,7 @@ import logging
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import AnonymousUser, Group
 from rest_framework.authentication import (BaseAuthentication,
                                            get_authorization_header)
 
@@ -18,8 +18,15 @@ class FederationAuthentication(BaseAuthentication):
         user["first_name"] = request.META['HTTP_X_USER_FIRSTNAME']
         user["last_name"] = request.META['HTTP_X_USER_LASTNAME']
         user["email"] = request.META['HTTP_X_USER_EMAIL']
-        user, __ = get_user_model().objects.get_or_create(
+        user, _ = get_user_model().objects.get_or_create(
             username=user["email"],
             defaults=user,
         )
-        return user
+        group = user.groups.all().first()
+        if not group:
+            group, _ = Group.objects.get_or_create(
+                name=request.META['HTTP_X_USER_GROUP']
+            )
+            user.groups.set([group])
+            user.save()
+        return user, group
